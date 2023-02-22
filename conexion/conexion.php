f110ecbc72ee2150c65f16e497e18cca1e86990a
Gustavo Juarez <33164842+gdjuarez@users.noreply.github.com>
2023-02-22 15:04:35 -0300

    Add files via upload

---
+++ conexion/conexion.php
@@ -0,0 +1,16 @@
+<?php
+ require_once('config.php');
+    
+    $miConexion = mysqli_connect( $db_host,  $db_usuario, $db_contra,$db_nombre);
+    
+    if (!$miConexion) {
+        echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+        echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+        echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+    
+   // echo "conectado Online!";
+ 
+?>
+
---
+++ conexion/config.php
@@ -0,0 +1,14 @@
+<?php
+  /*
+    $db_host="localhost";
+    $db_nombre="";
+    $db_usuario="";
+    $db_contra="";
+*/
+    $db_host="localhost";
+    $db_nombre="u345632271_pmo";
+    $db_usuario="root";
+    $db_contra="";
+
+ 
+?>
